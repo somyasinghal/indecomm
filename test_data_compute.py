1e0b18dfbe05279434b4fccf19cdd11b82a89785
somyasinghal <somya.singhal31@gmail.com>
2014-05-14 09:32:14 +0530

    test_data_compute.py
    test file for testing data_compute.py

---
+++ test_data_compute.py
@@ -0,0 +1,23 @@
+'''Unit test for test_data_compute.py
+
+This program tests the output computed by data_compute.compute_company_data
+with output getting compared with Expected output.
+'''
+
+import data_compute
+import unittest
+
+class ExpectedOutput(unittest.TestCase):
+    output_values = ( ['A', '1991', 'Dec', '240'],
+                      ['B', '1991', 'Sep', '300'],
+                      ['C', '1991', 'Jul', '200'],
+                      ['D', '1991', 'May', '101']                     
+                     )
+
+    def test_to_roman_known_values(self):
+        '''compute_company_data should give known result as output_values'''
+        result = data_compute.compute_company_data('sampledata.csv')
+        self.assertEqual(list(self.output_values), result)
+
+if __name__ == '__main__':
+    unittest.main()
